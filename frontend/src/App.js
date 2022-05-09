19d847fe810650ad712b5d9f43216a2684f02f7e
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-05-09 09:28:04 -0300

    Feat: My places Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-import NewPlace from './places/pages/NewPlace';
 import Users from './user/pages/Users';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path={`/:${userId}/places`} exact>
+            <UserPlaces />
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
